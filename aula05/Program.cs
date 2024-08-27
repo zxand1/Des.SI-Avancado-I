6d0daf497cee8dd1c8bfb0ae4047e507b4bcdfb2
Alexandre de Oliveira Pinheiro <105022657+zxand1@users.noreply.github.com>
2024-08-27 00:33:20 +0000

    Codigo da aula do dia 26

---
+++ aula05/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Class Program{
+    public static void Main(string[] args){
+        Console.WriteLine($"Int: {numInt}, Double: {numDouble}");
+
+
+        double valorDouble = 7.6;
+        int valorConvertidoInt = Convert.ToInt32(valueDouble);
+        string textoValor = "123";
+        int textoConvertInte = Convert.ToInt32(textoValor);
+        Console.WriteLine($"Double: {valorDouble}, Inteiro: {valorConvertidoInt}");
+        Console.WriteLine($"String: {textValor}, Inteiro: {valorConvertidoInt}");
+    }
+}
